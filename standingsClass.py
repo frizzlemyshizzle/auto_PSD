c310e3b7e5bc1e6e6214771509afd2c412065cd8
Jay Murphy (murphyj10) <murphyj10@coventry.ac.uk>
2022-07-19 16:29:11 +0100

    Initial commit. Table edits work via CSV in current version

---
+++ standingsClass.py
@@ -0,0 +1,34 @@
+franchises = {"SYMBIOTES":"SYM",
+            "SHADOW ESPORTS":"SE",
+            "LUNA ESPORTS":"LUNA",
+            "OVERRIDE":"OV",
+            "TEAM SERENE":"TS",
+            "ARCTIC FRENZY":"AF",
+            "DARK MAGIC":"DM",
+            "FORT TEMPLAR":"FT",
+            "SANCTUARY":"SANC",
+            "SHAMAN ESPORTS":"SHMN",
+            "SYSTEMA SOLARIS": "SOL",
+            "PANTHERA ESPORTS": "PNTR",
+            "ELEMENT ESPORTS":"LMNT",
+            "FEARLESS ESPORTS":"FRLS",
+            "GALAMO ESPORTS":"GALA",
+            "FORG":"FORG",
+            "MIGHT OF HADES":"MoH",
+            "WAIFU FAN CLUB":"UWU",
+            "OX GAMING":"OX",
+            "DSQ ESPORTS":"DSQ"}
+
+class standings:
+    franchOrder = []
+    teamOrder = []
+    gpOrder = []
+    gwOrder = []
+    glOrder = []
+    gsOrder = []
+    gcOrder = []
+    gdOrder = []
+    winPercOrder = []
+    ffWinOrder = []
+    ffLossOrder = []
+    recordOrder = []
