9f19f691e9ac4d0b2799f8eb9e57c67cba040982
Jay Murphy (murphyj10) <murphyj10@coventry.ac.uk>
2022-07-20 16:32:45 +0100

    Added teams to dicts

--- fixturesClass.py
+++ fixturesClass.py
@@ -24,25 +24,154 @@ franchises = {"ASCENCE ESPORTS": "ASC",
             "DSQ ESPORTS":"DSQ"}
 
 
+teamsPrem = {"ARCTIC FRENZY": "AF", 
+            "FEARLESS ESPORTS": "FRLS",
+            "CENTURIONS": "FT",
+            "PARADISE": "SANC",
+            "SHAMAN ESPORTS": "SHMN",
+            "DOMINION": "SYM",
+            "NAMI BRIGADE": "UWU",
+            "DUST SUCKER": "OX",
+            "GALAMO ESPORTS": "GALA",
+            "VULCAN": "",
+            "ALLHAMDOULILAH": "",
+            "ASCENCE ESPORTS": "",
+            "ATVINTA": "FORG",
+            "HAMOOD": "HAM",
+            "LEODIS": "LDS",
+            "PINGUINOS CF": "PCF",}
+
+teamsMaster = {"SUB-ZERO": "AF",
+            "NECROMANCERS": "DM",
+            "NOX": "DSQ", 
+            "EINSTEINIUM": "LMNT",
+            "FEARLESS ALPHA": "FRLS",
+            "GLADIATORS": "FT",
+            "HADES": "MoH",
+            "OVERWATCH": "OV",
+            "FENRIR": "LUNA",
+            "LEO": "PNTR",
+            "HAVEN": "SANC",
+            "ABYSS": "SE",
+            "IOVI": "SOL",
+            "SERENE NARA": "TS",
+            "DOGON": "SHMN",
+            "SEVERANCE": "SYM",
+            "RIAS LEGION": "UWU",
+            "HECK": "OX",
+            "OCEAN": "GALA",
+            "PHOENIX": "FORG"}
+
+teamsElite = {"BLIZZARD": "AF",
+            "WARLOCKS": "DM",
+            "CARBO": "DSQ", 
+            "ARGON": "LMNT",
+            "FEARLESS BRAVO": "FRLS",
+            "VANGUARDS": "FT",
+            "THANATOS": "MoH",
+            "OVERTAKE": "OV",
+            "EMBRY": "LUNA",
+            "TIGRIS": "PNTR",
+            "TEMPLE": "SANC",
+            "DARKNESS": "SE",
+            "SATURNIA": "SOL",
+            "SERENE AZUCHI": "TS",
+            "ZULU": "SHMN",
+            "ANARCHY": "SYM",
+            "MEGUMIN CULT": "UWU",
+            "ANJOU": "OX",
+            "EROS": "GALA",
+            "BLADESMITHS": "FORG"}
+
+teamsRival = {"AVALANCHE": "AF",
+            "SORCERERS": "DM",
+            "CALIGO": "DSQ", 
+            "NITROGEN": "LMNT",
+            "FEARLESS CHARLIE": "FRLS",
+            "SENTINELS": "FT",
+            "EREBOS": "MoH",
+            "OVERKILL": "OV",
+            "RAIJU": "LUNA",
+            "ONCA": "PNTR",
+            "SANCTUM": "SANC",
+            "NIGHTFALL": "SE",
+            "URANUM": "SOL",
+            "SERENE MEIJI": "TS",
+            "YORUBA": "SHMN",
+            "DISORDER": "SYM",
+            "ERINA TRIBE": "UWU",
+            "MONTBELIARDE": "OX",
+            "PANDORA": "GALA",
+            "JOURNEYMEN": "FORG"}
+
+teamsChall = {"GLACIER": "AF",
+            "PYROMANCERS": "DM",
+            "CANENS": "DSQ", 
+            "HELIUM": "LMNT",
+            "FEARLESS DELTA": "FRLS",
+            "CRUSADERS": "FT",
+            "ORCUS": "MoH",
+            "OVERRULE": "OV",
+            "CADEJO": "LUNA",
+            "PARDUS": "PNTR",
+            "SHRINE": "SANC",
+            "ECLIPSE": "SE",
+            "NEPTUNIUM": "SOL",
+            "SERENE YAMATO": "TS",
+            "MAASAI": "SHMN",
+            "PRIMORDIAL": "SYM",
+            "NEZUKO GANG": "UWU",
+            "PARTHENAIS": "OX",
+            "CEREBRUS": "GALA",
+            "APPRENTICES": "FORG"}
+
+teamsProsp = {"ICICLE": "AF",
+            "GEOMANCERS": "DM",
+            "LIVENS": "DSQ", 
+            "OXYGEN": "LMNT",
+            "FEARLESS ECHO": "FRLS",
+            "CARDINALS": "FT",
+            "TARTARUS": "MoH",
+            "OVERTURN": "OV",
+            "ANUBIS": "LUNA",
+            "JUBATUS": "PNTR",
+            "ALTAR": "SANC",
+            "OBSCURITY": "SE",
+            "TERRA": "SOL",
+            "SERENE TAISHO": "TS",
+            "KARO": "SHMN",
+            "PARASITE": "SYM",
+            "MAIKA FACTION": "UWU",
+            "LIMOUSIN": "OX",
+            "PLUTON": "GALA",
+            "HOT METAL": "FORG"}
+
+
+
 class prem:
     teamOrder = []
     dateOrder = []
     timeOrder = []
+    logoOrder = []
 
 class master:
     teamOrder = []
     dateOrder = []
     timeOrder = []
+    logoOrder = []
+
 
 class elite:
     teamOrder = []
     dateOrder = []
     timeOrder = []
+    logoOrder = []
 
 class rival:
     teamOrder = []
     dateOrder = []
     timeOrder = []
+    logoOrder = []
 
 class chall:
     teamOrder = []
@@ -53,6 +182,4 @@ class prosp:
     teamOrder = []
     dateOrder = []
     timeOrder = []
-    
-
-
+    logoOrder = []
