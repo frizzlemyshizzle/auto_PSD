49f35001df1158027fd911a83e58837653a45e2d
Jay Murphy (murphyj10) <murphyj10@coventry.ac.uk>
2022-07-20 12:47:35 +0100

    Added Premier teams and Tier classes

--- standingsClass.py
+++ standingsClass.py
@@ -1,4 +1,8 @@
-franchises = {"SYMBIOTES":"SYM",
+franchises = {"ASCENCE ESPORTS": "ASC",
+            "HAMOOD": "HAM",
+            "LEODIS": "LDS",
+            "PINGUINOS CF": "PCF",
+            "SYMBIOTES":"SYM",
             "SHADOW ESPORTS":"SE",
             "LUNA ESPORTS":"LUNA",
             "OVERRIDE":"OV",
@@ -19,7 +23,77 @@ franchises = {"SYMBIOTES":"SYM",
             "OX GAMING":"OX",
             "DSQ ESPORTS":"DSQ"}
 
-class standings:
+class PremStandings:
+    franchOrder = []
+    teamOrder = []
+    gpOrder = []
+    gwOrder = []
+    glOrder = []
+    gsOrder = []
+    gcOrder = []
+    gdOrder = []
+    winPercOrder = []
+    ffWinOrder = []
+    ffLossOrder = []
+    recordOrder = []
+
+class MasterStandings:
+    franchOrder = []
+    teamOrder = []
+    gpOrder = []
+    gwOrder = []
+    glOrder = []
+    gsOrder = []
+    gcOrder = []
+    gdOrder = []
+    winPercOrder = []
+    ffWinOrder = []
+    ffLossOrder = []
+    recordOrder = []
+
+class EliteStandings:
+    franchOrder = []
+    teamOrder = []
+    gpOrder = []
+    gwOrder = []
+    glOrder = []
+    gsOrder = []
+    gcOrder = []
+    gdOrder = []
+    winPercOrder = []
+    ffWinOrder = []
+    ffLossOrder = []
+    recordOrder = []
+
+class RivalStandings:
+    franchOrder = []
+    teamOrder = []
+    gpOrder = []
+    gwOrder = []
+    glOrder = []
+    gsOrder = []
+    gcOrder = []
+    gdOrder = []
+    winPercOrder = []
+    ffWinOrder = []
+    ffLossOrder = []
+    recordOrder = []
+
+class ChallStandings:
+    franchOrder = []
+    teamOrder = []
+    gpOrder = []
+    gwOrder = []
+    glOrder = []
+    gsOrder = []
+    gcOrder = []
+    gdOrder = []
+    winPercOrder = []
+    ffWinOrder = []
+    ffLossOrder = []
+    recordOrder = []
+
+class ProspStandings:
     franchOrder = []
     teamOrder = []
     gpOrder = []
