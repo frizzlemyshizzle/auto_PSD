68541cbfc9440bd634d4384617b4a38fa9157fc0
Jay Murphy (murphyj10) <murphyj10@coventry.ac.uk>
2022-07-20 15:01:02 +0100

    Added tier classes

---
+++ fixturesClass.py
@@ -0,0 +1,58 @@
+franchises = {"ASCENCE ESPORTS": "ASC",
+            "HAMOOD": "HAM",
+            "LEODIS": "LDS",
+            "PINGUINOS CF": "PCF",
+            "SYMBIOTES":"SYM",
+            "SHADOW ESPORTS":"SE",
+            "LUNA ESPORTS":"LUNA",
+            "OVERRIDE":"OV",
+            "TEAM SERENE":"TS",
+            "ARCTIC FRENZY":"AF",
+            "DARK MAGIC":"DM",
+            "FORT TEMPLAR":"FT",
+            "SANCTUARY":"SANC",
+            "SHAMAN ESPORTS":"SHMN",
+            "SYSTEMA SOLARIS": "SOL",
+            "PANTHERA ESPORTS": "PNTR",
+            "ELEMENT ESPORTS":"LMNT",
+            "FEARLESS ESPORTS":"FRLS",
+            "GALAMO ESPORTS":"GALA",
+            "FORG":"FORG",
+            "MIGHT OF HADES":"MoH",
+            "WAIFU FAN CLUB":"UWU",
+            "OX GAMING":"OX",
+            "DSQ ESPORTS":"DSQ"}
+
+
+class prem:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+
+class master:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+
+class elite:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+
+class rival:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+
+class chall:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+
+class prosp:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+    
+
+
