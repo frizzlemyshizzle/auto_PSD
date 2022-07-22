6c3abccb9c833207756b351d80eaa31fecea224c
Jay Murphy (murphyj10) <murphyj10@coventry.ac.uk>
2022-07-22 13:26:16 +0100

    Changed class files

---
+++ fixturesClass.py
@@ -0,0 +1,187 @@
+tiers = ['Premier', 'Master', 'Elite', 'Rival', 'Challenger', 'Prospect']
+
+franchises = {"ASCENCE ESPORTS": "ASC",
+            "HAMOOD": "HAM",
+            "LEODIS": "LDS",
+            "PINGUINOS CF": "PCF",
+            "SYMBIOTES":"SYM",
+            "SHADOW ESPORTS":"SE",
+            "LUNA ESPORTS":"LUNA",
+            "OVERRIDE":"OV",
+            "TEAM SERENE":"TS",
+            "ARCTIC FRENZY":"AF",
+            "DARK MAGIC":"DM",
+            "FORT TEMPLAR":"FT",
+            "SANCTUARY":"SANC",
+            "SHAMAN ESPORTS":"SHMN",
+            "SYSTEMA SOLARIS": "SOL",
+            "PANTHERA ESPORTS": "PNTR",
+            "ELEMENT ESPORTS":"LMNT",
+            "FEARLESS ESPORTS":"FRLS",
+            "GALAMO ESPORTS":"GALA",
+            "FORG":"FORG",
+            "MIGHT OF HADES":"MoH",
+            "WAIFU FAN CLUB":"UWU",
+            "OX GAMING":"OX",
+            "DSQ ESPORTS":"DSQ"}
+
+teamsPrem = {"ARCTIC FRENZY": "AF", 
+            "FEARLESS ESPORTS": "FRLS",
+            "CENTURIONS": "FT",
+            "PARADISE": "SANC",
+            "SHAMAN ESPORTS": "SHMN",
+            "DOMINION": "SYM",
+            "NAMI BRIGADE": "UWU",
+            "DUST SUCKER": "OX",
+            "GALAMO ESPORTS": "GALA",
+            "VULCAN": "FORG",
+            "ALHAMDOULILAH": "ALH",
+            "ASCENCE ESPORTS": "ASC",
+            "ATVINTA": "FORG",
+            "HAMOOD": "HAM",
+            "LEODIS": "LDS",
+            "PINGUINOS CF": "PCF"}
+
+teamsMaster = {"SUB-ZERO": "AF",
+            "NECROMANCERS": "DM",
+            "NOX": "DSQ", 
+            "EINSTEINIUM": "LMNT",
+            "FEARLESS ALPHA": "FRLS",
+            "GLADIATORS": "FT",
+            "HADES": "MoH",
+            "OVERWATCH": "OV",
+            "FENRIR": "LUNA",
+            "LEO": "PNTR",
+            "HAVEN": "SANC",
+            "ABYSS": "SE",
+            "IOVI": "SOL",
+            "SERENE NARA": "TS",
+            "DOGON": "SHMN",
+            "SEVERANCE": "SYM",
+            "RIAS LEGION": "UWU",
+            "HECK": "OX",
+            "OCEAN": "GALA",
+            "PHOENIX": "FORG"}
+
+teamsElite = {"BLIZZARD": "AF",
+            "WARLOCKS": "DM",
+            "CARBO": "DSQ", 
+            "ARGON": "LMNT",
+            "FEARLESS BRAVO": "FRLS",
+            "VANGUARDS": "FT",
+            "THANATOS": "MoH",
+            "OVERTAKE": "OV",
+            "EMBRY": "LUNA",
+            "TIGRIS": "PNTR",
+            "TEMPLE": "SANC",
+            "DARKNESS": "SE",
+            "SATURNIA": "SOL",
+            "SERENE AZUCHI": "TS",
+            "ZULU": "SHMN",
+            "ANARCHY": "SYM",
+            "MEGUMIN CULT": "UWU",
+            "ANJOU": "OX",
+            "EROS": "GALA",
+            "BLADESMITHS": "FORG"}
+
+teamsRival = {"AVALANCHE": "AF", 
+            "SORCERERS": "DM",
+            "CALIGO": "DSQ", 
+            "NITROGEN": "LMNT",
+            "FEARLESS CHARLIE": "FRLS",
+            "SENTINELS": "FT",
+            "EREBOS": "MoH",
+            "OVERKILL": "OV",
+            "RAIJU": "LUNA",
+            "ONCA": "PNTR",
+            "SANCTUM": "SANC",
+            "NIGHTFALL": "SE",
+            "URANUM": "SOL",
+            "SERENE MEIJI": "TS",
+            "YORUBA": "SHMN",
+            "DISORDER": "SYM",
+            "ERINA TRIBE": "UWU",
+            "MONTBELIARDE": "OX",
+            "PANDORA": "GALA",
+            "JOURNEYMEN": "FORG"}
+
+teamsChall = {"GLACIER": "AF",
+            "PYROMANCERS": "DM",
+            "CANENS": "DSQ", 
+            "HELIUM": "LMNT",
+            "FEARLESS DELTA": "FRLS",
+            "CRUSADERS": "FT",
+            "ORCUS": "MoH",
+            "OVERRULE": "OV",
+            "CADEJO": "LUNA",
+            "PARDUS": "PNTR",
+            "SHRINE": "SANC",
+            "ECLIPSE": "SE",
+            "NEPTUNIUM": "SOL",
+            "SERENE YAMATO": "TS",
+            "MAASAI": "SHMN",
+            "PRIMORDIAL": "SYM",
+            "NEZUKO GANG": "UWU",
+            "PARTHENAIS": "OX",
+            "CEREBRUS": "GALA",
+            "APPRENTICES": "FORG"}
+
+teamsProsp = {"ICICLE": "AF",
+            "GEOMANCERS": "DM",
+            "LIVENS": "DSQ", 
+            "OXYGEN": "LMNT",
+            "FEARLESS ECHO": "FRLS",
+            "CARDINALS": "FT",
+            "TARTARUS": "MoH",
+            "OVERTURN": "OV",
+            "ANUBIS": "LUNA",
+            "JUBATUS": "PNTR",
+            "ALTAR": "SANC",
+            "OBSCURITY": "SE",
+            "TERRA": "SOL",
+            "SERENE TAISHO": "TS",
+            "KARO": "SHMN",
+            "PARASITE": "SYM",
+            "MAIKA FACTION": "UWU",
+            "LIMOUSIN": "OX",
+            "PLUTON": "GALA",
+            "HOT METAL": "FORG"}
+
+
+
+class prem:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+    logoOrder = []
+
+class master:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+    logoOrder = []
+
+
+class elite:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+    logoOrder = []
+
+class rival:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+    logoOrder = []
+
+class chall:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+    logoOrder = []
+
+class prosp:
+    teamOrder = []
+    dateOrder = []
+    timeOrder = []
+    logoOrder = []
