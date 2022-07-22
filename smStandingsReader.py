2fa9d6ff9fa7824dfedfd6cfd636d200d8ebc3ea
Jay Murphy (murphyj10) <murphyj10@coventry.ac.uk>
2022-07-22 15:11:03 +0100

    Final Version

--- smStandingsReader.py
+++ smStandingsReader.py
@@ -1,13 +1,18 @@
 import gspread
 import configparser
 import fixturesClass as fc
+import win32com.client
+import os
+
 #################
 # Config Reader #
 #################
 config = configparser.ConfigParser()
 config.read_file(open(r"SMStandingsConfig.txt"))
+configWeek = config.get('config', 'Week')
 configTier = config.get('config', 'Tier')
 configConf = config.get('config', 'Conference')
+
 
 ###############
 # Class Setup #
@@ -18,13 +23,21 @@ class standings:
     abbrOrder = []
 
 
-#########
-# Setup #
-#########
+############
+# GS Setup #
+############
 sa = gspread.service_account(filename="service_account.json")
 sh = sa.open("RSC10 | Graphics Data")
 wks = sh.worksheet("TableOutput")
 
+# PNG Options
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+pngFile = (os.path.join(__location__, 'Outputs/SMStandings.png'))
+options = win32com.client.Dispatch('Photoshop.ExportOptionsSaveForWeb')
+options.Format = 13
+options.quality = 100
+options.PNG8 = False  # Sets it to PNG-24 bit
 
 offset = 1
 premCount = 0
@@ -55,16 +68,16 @@ premCount += offset
 ##########################
 # Sheet Indexes 19/07/22 #
 ##########################
-premIdx = str("C2:F{}".format(premCount))
-masterIdx = str("C{}:F{}".format(premCount + 1, premCount + masterCount))
-eliteIdx = str("C{}:F{}".format(premCount + masterCount + 1, premCount +
+premIdx = str("B2:F{}".format(premCount))
+masterIdx = str("B{}:F{}".format(premCount + 1, premCount + masterCount))
+eliteIdx = str("B{}:F{}".format(premCount + masterCount + 1, premCount +
                                 masterCount + eliteCount))
-rivalIdx = str("C{}:F{}".format(premCount + masterCount + eliteCount + 1,
+rivalIdx = str("B{}:F{}".format(premCount + masterCount + eliteCount + 1,
                                 premCount + masterCount + eliteCount +  rivalCount))
-challIdx = str("C{}:F{}".format(premCount + masterCount + eliteCount + rivalCount + 1,
+challIdx = str("B{}:F{}".format(premCount + masterCount + eliteCount + rivalCount + 1,
                                 premCount + masterCount + eliteCount +  rivalCount +
                                 challCount))
-prospIdx = str("C{}:F{}".format(premCount + masterCount + eliteCount + rivalCount + 
+prospIdx = str("B{}:F{}".format(premCount + masterCount + eliteCount + rivalCount + 
                                 challCount + 1,
                                 premCount + masterCount + eliteCount +  rivalCount +
                                 challCount + prospCount))     
@@ -85,27 +98,86 @@ prospData = wks.get(prospIdx)
 if configTier == 'Premier':
     tierData = premData
     tierClass = fc.teamsPrem
+    teamsConf = (premCount - offset)//2
 if configTier == 'Master':
     tierData = masterData
     tierClass = fc.teamsMaster
+    teamsConf = masterCount//2
 if configTier == 'Elite':
     tierData = eliteData
     tierClass = fc.teamsElite
+    teamsConf = eliteCount//2
 if configTier == 'Rival':
     tierData = rivalData
     tierClass = fc.teamsRival
+    teamsConf = rivalCount//2
 if configTier == 'Challenger':
     tierData = challData
-    tierClass = fc.teamsChall
+    tierClass = fc.teamsChall//2
+    teamsConf = challCount//2
 if configTier == 'Prospect':
     tierData = prospData
     tierClass = fc.teamsProsp
+    teamsConf = prospCount//2
 
 
 for row in tierData:
-    standings.teamOrder.append(str(row[1]).upper())
-    standings.winsOrder.append(str(row[3]).upper())
+    if configConf == "Glacies":
+        if row[0] == "Glacies":
+            standings.teamOrder.append(str(row[2]).upper())
+            standings.winsOrder.append(str(row[4]).upper())
+    if configConf == "Ignis":
+        if row[0] == "Ignis":
+            standings.teamOrder.append(str(row[2]).upper())
+            standings.winsOrder.append(str(row[4]).upper())
+
 for team in standings.teamOrder:
     standings.abbrOrder.append(tierClass[team])
 
-print(standings.abbrOrder)
+            
+
+
+
+
+def editTable(teamsConf):
+    psApp = win32com.client.Dispatch("Photoshop.Application")
+    psApp.Open(r"C:\Users\jaymu\Desktop\RSC\psd_editor\SMStandings.psd")
+    doc = psApp.Application.ActiveDocument 
+    titlegroup = doc.activeLayer = (doc.layerSets["Title Banner"])
+    weekNum = titlegroup.ArtLayers["WeekNum"]
+    weeknumText = weekNum.textItem
+    weeknumText.contents = "WEEK {} | {}".format(str(configWeek), str(configConf).upper())
+    bannerGroup = titlegroup.layerSets[configTier]
+    bannerGroup.visible = True
+
+    loopCount = 0
+    while loopCount < teamsConf:
+        # Edit Team Names
+        teamGroup =  doc.activeLayer = (doc.layerSets["Team {}".format(loopCount+1)])
+        teamName = teamGroup.ArtLayers["TeamName"]
+        teamNameText = teamName.textItem
+        teamNameText.contents = standings.teamOrder[loopCount]
+        # Edit Team Wins
+        teamWins = teamGroup.ArtLayers["Wins"]
+        teamWinsText = teamWins.textItem
+        teamWinsText.contents = standings.winsOrder[loopCount]
+        # Edit Team Logos
+        teamLogoGroup = teamGroup.LayerSets["Logo"]
+        teamLogo = teamLogoGroup.ArtLayers[standings.abbrOrder[loopCount]]
+        teamLogo.visible = True
+        loopCount += 1
+
+    if configTier == 'Premier':
+        extraTeamGroup =  doc.activeLayer = (doc.layerSets["Team 9"])
+        extraTeamGroup.visible = False
+        extraTeamGroup2 =  doc.activeLayer = (doc.layerSets["Team 10"])
+        extraTeamGroup2.visible = False
+    doc.Export(ExportIn=pngFile, ExportAs=2, Options=options)
+    doc.Close(2)
+    
+    
+
+
+    
+
+editTable(teamsConf)
